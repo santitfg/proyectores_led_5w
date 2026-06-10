668682039dba436f032d133f44e7d45efb2441ed
santi <stfg.prof@gmail.com>
2026-06-10 11:22:42 -0300

    sumando demos funconales

--- multiplexar_alto_voltaje_simple_74hc695/multiplexar_alto_voltaje_simple_74hc695.ino
+++ multiplexar_alto_voltaje_simple_74hc695/multiplexar_alto_voltaje_simple_74hc695.ino
@@ -3,10 +3,10 @@
 // pata en alto permite el paso de correinte
 int pinesColumnas[2] = { 3, 4 };
 int nColumnas = 0;
-int nFilas = 0
-  // Filas Canal P - IRF9630 _ lado alto de cambio
-  // pata en bajo permite el paso de correinte
-  int pinesFilas[2] = { 8, 9 };
+int nFilas = 0;
+// Filas Canal P - IRF9630 _ lado alto de cambio
+// pata en bajo permite el paso de correinte
+int pinesFilas[2] = { 8, 9 };
 
 void setup() {
   nColumnas = sizeof(pinesColumnas) / sizeof(pinesColumnas[0]);
@@ -24,9 +24,12 @@ void loop() {
 
   for (int i = 0; i < nColumnas; i++) {
     for (int j = 0; j < nFilas; j++) {
-      digitalWrite(pinesColumnas[j],HIGH);
-      digitalWrite(pinesFilas[j],HIGH);
+      digitalWrite(pinesColumnas[i], HIGH);
+      digitalWrite(pinesFilas[j], HIGH);
       delay(600);
+      digitalWrite(pinesColumnas[i], LOW);
+      digitalWrite(pinesFilas[j], LOW);
+      delay(200);
     }
   }
   // largoBits=
